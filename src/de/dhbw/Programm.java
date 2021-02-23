ef711261727aac06843ec8f76d4b75140f851145
Jan-Niklas <jan.niklas.koenig@gmail.com>
2021-02-23 01:03:29 +0100

    Hat funktioniert?

--- src/de/dhbw/Programm.java
+++ src/de/dhbw/Programm.java
@@ -4,6 +4,6 @@ public class Programm {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Test");
+        System.out.println("Commit hat funktioniert?");
     }
 }
