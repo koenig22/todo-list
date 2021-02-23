0a401861cd807c419bd966aeb4391746e507169b
Jan-Niklas <jan.niklas.koenig@gmail.com>
2021-02-23 14:31:15 +0100

    Update Programm.java
    Commit Test Vorführeffekt

--- src/de/dhbw/Programm.java
+++ src/de/dhbw/Programm.java
@@ -5,6 +5,6 @@ public class Programm {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Commit hat funktioniert?");
-	System.out.println("Test");
+	System.out.println("Test commit");
     }
 }
