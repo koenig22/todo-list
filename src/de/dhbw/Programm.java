e35b244cdb7b12d7efa82141cdca2d201ea9f155
Jan-Niklas <jan.niklas.koenig@gmail.com>
2021-02-23 14:22:57 +0100

    Abänderung

--- src/de/dhbw/Programm.java
+++ src/de/dhbw/Programm.java
@@ -5,6 +5,6 @@ public class Programm {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Commit hat funktioniert?");
-	System.out.println("Wuhu es hat funtioniert");
+	System.out.println("Test");
     }
 }
